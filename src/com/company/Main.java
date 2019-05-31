82fac3a68ee7fc80d6b00572138d087fc5c4bbd4
otchlan <raf.szuwalski@gmail.com>
2019-05-31 20:46:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/home/taicojeszcze/Programy/SElenium/chromedriver");
+
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless");
+//        chromeOptions.addArguments("--no-sandbox");
+//
+//        WebDriver driver = new ChromeDriver(chromeOptions);
+//
+//        driver.get("https://google.com");
+//
+//        Thread.sleep(1000);
+//
+//        if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+//            System.out.println("Pass");
+//        } else {
+//            System.out.println("Fail");
+//        }
+//        driver.quit();
+
+    WebDriver driver = new ChromeDriver();
+
+    driver.get("https://google.com");
+
+    }
+}
